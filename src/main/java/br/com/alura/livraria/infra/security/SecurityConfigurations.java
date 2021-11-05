1e609fb838949d79e1f82acf49a23f8997066b95
Alexandre Mattos <alexandrerio.br@gmail.com>
2021-11-05 11:11:26 -0300

    Alterando autenticacao para stateless via token

--- src/main/java/br/com/alura/livraria/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/livraria/infra/security/SecurityConfigurations.java
@@ -1,10 +1,15 @@
 package br.com.alura.livraria.infra.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -21,15 +26,38 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
         auth
         .userDetailsService(autenticacaoService)
         .passwordEncoder(bCryptPasswordEncoder);
+        //.passwordEncoder(new BCryptPasswordEncoder());
     }
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
         .authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/auth").permitAll()
         .anyRequest().authenticated()
-        .and().formLogin()
+        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and().csrf().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // Liberando acesso a recursos estaticos
+        web
+        .ignoring()
+        .antMatchers(
+                "/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**"
+                );
+    }
+    
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
