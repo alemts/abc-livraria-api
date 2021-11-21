aec0f77d0e2a568f4994ec2b1019f3733008911e
Alexandre Mattos <alexandrerio.br@gmail.com>
2021-11-20 22:06:55 -0300

    Exclusao das migrations

--- src/main/resources/db/migration/V1__create-table-livros.sql
+++
@@ -1,7 +0,0 @@
-create table livros(
-  Id bigint not null auto_increment,
-  Titulo varchar(100) not null,
-  Data_Lancamento date not null,
-  Numero_Paginas int not null,
-  primary key(id)
-);
--- src/main/resources/db/migration/V6__alter-table-livros-add-column-usuario-id.sql
+++
@@ -1,5 +0,0 @@
-alter table livros
-  add column usuario_id bigint;
-  
-alter table livros
-  add foreign key(usuario_id) references usuarios(Id);
